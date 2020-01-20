11bfd003bef42e6b5ab2f80f6ad2556e11f65111
Andrew Nordlund <andrew.nordlund@hrsdc-rhdcc.gc.ca>
2020-01-20 14:11:46 -0500

    WIP:Gathering content.  Gotta figure out how to validate and get results.

--- content_scripts/nordValidator-cs.js
+++ content_scripts/nordValidator-cs.js
@@ -1,10 +1,93 @@
-var nordcmxCS = {
+var nordValidatorCS = {
 	dbug : nordValidator.dbug,
 	loggedIn : false,
 	usingTmp : false,
+	stat : null,
 	init : function () {
+		// Should I do something here?
+		//
+		nordValidatorCS.startProcess();
 	}, // End of init
-	
+	startProcess : function () {
+		// gather contents
+		// Two options here:
+		// Do like the bookmarklet and create a textnode and form to submit to the validator, or
+		// Gather the contents into a variable, send to the BG script, and have it send the data.  This one may be the necessary one.
+		
+		var body, validForm, showSource, contentTA = null;
+		var contents = "";
+		contents = nordValidatorCS.gatherContent();
+		body = document.getElementsByTagName("body")[0];
+
+		validForm = document.getElementById("validForm");
+		showSource = document.getElementById("showSource");
+		contentTA = document.getElementById("contentTA");
+
+		if (!validForm) validForm = nordburg.createHTMLElement(document, "form", {"method":"POST", "action":nordValidator.options.validatorURL, "enctype":"multipart/form-data", "acceptCharset":"utf-8","parentNode":body, "id":"validForm"});
+		if (!showSource) showSource = nordburg.createHTMLElement(document, "input", {"type":"hidden", "id":"showSource", "name":"showsource", "value":"yes", "parentNode":validForm});
+		if (!contentTA) contentTA = nordburg.createHTMLElement(document, "textarea", {"textNode":contents, "parentNode":validForm, "name":"content", "id":"contentTA"});
+
+
+		
+/*
+(function(){
+	function c(a,b) {
+		var c=document.createElement("textarea");
+		c.name=a;
+		c.value=b;
+		d.appendChild(c)
+	}
+	var e=function(a) { 
+		for (var b="",a=a.firstChild; a; ) {
+			switch (a.nodeType) {
+				case Node.ELEMENT_NODE:b+=a.outerHTML;break;
+				case Node.TEXT_NODE:b+=a.nodeValue;break;
+				case Node.CDATA_SECTION_NODE:b+="<![CDATA["+a.nodeValue+"]]\>";break;
+				case Node.COMMENT_NODE:b+="<\!--"+a.nodeValue+"--\>";break;
+				case Node.DOCUMENT_TYPE_NODE:b+="<!DOCTYPE "+a.name+">\n"
+			}
+			a=a.nextSibling
+		}return b
+	}(document),d=document.createElement("form");
+
+	d.method="POST";
+	d.action="https://validator.w3.org/nu/";
+	d.enctype="multipart/form-data";
+	d.target="_blank";
+	d.acceptCharset="utf-8";
+
+	c("showsource","yes");
+	c("content",e);
+	document.body.appendChild(d);d.submit();d.parentNode.removeChild(d);})();
+		*/
+		// Send for Validation
+		
+		// Set stat
+		
+		// Tell the boss to change the icon
+	}, // End of startProcess
+	gatherContent : function () {
+		var contents=[];
+		console.log ("firstSibling: "  + document.firstChild.nodeType +", and childNodes[0]: " + document.childNodes[0].nodeType + ".");
+		for (var i = 0; i < document.childNodes.length; i++) {
+			if (i == 0) console.log ("i:0 nodeType: " + document.childNodes[i].nodeType);
+			if (document.childNodes[i].nodeType == Node.ELEMENT_NODE) {
+				contents.push(document.childNodes[i].outerHTML);
+			} else if (document.childNodes[i].nodeType == Node.TEXT_NODE) {
+				contents.push(document.childNodes[i].nodeValue);
+			} else if (document.childNodes[i].nodeType == 10) {
+				console.log ("Found doctype!");
+				contents.push("<!DOCTYPE "+document.childNodes[i].name+">");
+			}
+		}
+		//contents = document.innerHTML;
+		//console.log ("contents: "  + contents +".");
+		//console.log ((body && contents
+		//if (nordValidator.dbug) console.log ("Got contents: " + contents + ".");
+
+		return contents.join("\n");
+
+	}, // End of gatherContent
 	startup : function () {
 	}, // End of startup
 	notify : function (message) {
@@ -16,7 +99,7 @@ var nordcmxCS = {
 		*/
 	}, // End of notify
 }
-
+/*
 document.addEventListener("DOMContentLoaded", function () {
 	if (nordValidatorCS.dbug) console.log ("Content loaded, getting stored stuff.");
 	//nordValidator.getSaved(nordValidatorCS.startup, nordValidator.errorFun);
@@ -46,9 +129,12 @@ document.addEventListener("DOMContentLoaded", function () {
 	}, nordValidator.errorFun);
 		}
 }, false);
+*/
 browser.runtime.onMessage.addListener(nordValidatorCS.notify);
 if (nordValidatorCS.dbug) console.log ("nordValidatorCS.js loaded.");
 nordValidator.addToPostLoad([function () {
 	if (nordValidatorCS.dbug === false && nordValidator.dbug === true) console.log ("turning nordValidatorCS.dbug on.");
 	nordValidatorCS.dbug = nordValidator.dbug;
 }]);
+
+if (document.location.href.match(/http/i)) nordValidatorCS.init();
--- nordValidator-bg.js
+++ nordValidator-bg.js
@@ -6,17 +6,38 @@ var nordValidatorBG = {
 	dbug : nordValidator.dbug,
 	init : function () {
 	}, // End of init
+	changeIcon : function (stat) {
+		if (nordValidatorBG.dbug) console.log ("changeIcon::About to change the icon.");
+		browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		
+			switch (stat) {
+				case "waiting":
+					browser.browserAction.setIcon({path: "icons/waiting-19.png", tabId: tabs[0].id});
+					break;
+				case "valid":
+					browser.browserAction.setIcon({path: "icons/nordValidator-19.png", tabId: tabs[0].id});
+					break;
+				case "invalid":
+					browser.browserAction.setIcon({path: "icons/notValid-19.png", tabId: tabs[0].id});
+					break;
+				case "error":
+					browser.browserAction.setIcon({path: "icons/err-19.png", tabId: tabs[0].id});
+					break;
+			}
+		});
+	}, // End of changeIcon
 	notify : function (message, sender, sendResponse) {
-		if (nordValidatorBG.dbug) console.log ("nordValidator-bg::Got a message: " + (message.hasOwnProperty("msg") ? message["msg"] : "[no msg]") + " with task: " + message["task"] + " with sender " + sender + " and sendResponse " + sendResponse + ".");
-		if (message["task"] == "someTask") {
-			if (nordValidatorBG.dbug) console.log ("nordValidator-bg::Gonna find a comic on " + message["pageURL"] + ".");
+		if (nordValidatorBG.dbug) console.log ("nordValidatorBG::Got a message: " + (message.hasOwnProperty("msg") ? message["msg"] : "[no msg]") + " with task: " + message["task"] + " with sender " + sender + " and sendResponse " + sendResponse + ".");
+		if (message["task"] == "changeIcon") {
+			if (nordValidatorBG.dbug) console.log ("nordValidatorBG::Gonna change icon to " + message["status"] + ".");
+			nordValidatorBG.changeIcon(message["status"]);
 		} else if (message["task"] == "updateOptions") {
 			nordValidator.options = message["options"];
 			nordValidatorBG.dbug = nordValidator.options["dbug"];
 			nordValidator.dbug = nordValidator.options["dbug"];
-		} else if (message["task"] == "close") {
+		} /*else if (message["task"] == "close") {
 			// close the window
-			var q = browser.tabs.query({title : "/* Some title */"});
+			var q = browser.tabs.query({title : "/* Some title * /"});
 			q.then(function (tabs) {
 				if (nordValidatorBG.dbug) console.log ("nordValidator-bg::tabs: " + tabs.length + ".");
 				for (var t = 0; t < tabs.length; t++) {
@@ -26,11 +47,31 @@ var nordValidatorBG = {
 			}, nordValidator.errorFun);
 		} else {
 			// Some default thing
-		}
+		} */
 	} // End of notify
 }
 
-browser.browserAction.onClicked.addListener(nordValidatorBG.initTab);
+var activeListenerFun = function (activeInfo) {
+	console.log ("Active!");
+	nordValidatorBG.changeIcon("waiting");
+}
+
+browser.tabs.onActivated.addListener(activeListenerFun);
+
+//browser.tabs.onActivated.addListener(function (activeInfo) {
+	//nordValidatorBG.changeIcon("waiting");
+	/*if (dbug) console.log ("nordValidatorBG::tab (" + activeInfo.tabId + "/" + activeInfo.windowId + ") has been activated.");
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		if (tabs[0]) {
+			if (dbug) console.log ("nordValidatorBG::sending message to get validity.");
+			browser.tabs.sendMessage(tabs[0].id, {"task": "getStatus"}).then(changeTitle, null);
+		}
+	});
+		//run();
+	*/
+//});
+
+browser.browserAction.onClicked.addListener(nordValidatorBG.init);
 browser.runtime.onMessage.addListener(nordValidatorBG.notify);
 nordValidator.addToPostLoad([function () {
 	if (nordValidatorBG.dbug) console.log ("setting nordValidatorBG.dbug to " + nordValidator.dbug + ".");
